3df64d391c1ca137cc72f91869b08e31de37809d
Zuhair Shad <123764183+Zuhairshad@users.noreply.github.com>
2025-09-15 20:40:04 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,24 @@ import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
+  base: '/',  
+
   plugins: [
     react(),
     visualizer({
-      filename: 'stats.html',    // file will be created in project root
-      template: 'treemap',       // or 'sunburst' if you like
+      filename: 'stats.html',   // file will be created in project root
+      template: 'treemap',      // or 'sunburst'
       gzipSize: true,
       brotliSize: true,
-      open: true                 // auto-open after build
+      open: false               // set to true if you want auto-open locally
     })
   ],
+
   server: {
     port: 3000
   },
+
   build: {
-    sourcemap: true              // makes it easier to debug bundle size
+    sourcemap: true             // helpful for debugging
   }
 })
